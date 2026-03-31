7b8c44239b90b7cb124469405d0fbab7cf8d5411
0xsajed <sajed0osama2010@gmail.com>
2026-04-01 00:23:10 +0200

    Update 1st-tool.py
    this is the 1st update to this very humble tool, I will work on it and explore multiple possible features to add. 31/03/2026

--- 1st-tool.py
+++ 1st-tool.py
@@ -1,10 +1,24 @@
-import requests
+import request
 target_url = str(input('Insert Url: '))
-headers = ['server', 'X-Frame-Options', 'X-Powered-By', 'Strict-Transport-Security', 'X-Content-Type-Options', 'Content-Security-Policy' ]
+while target_url == '' or not target_url.startswith(('http://', 'https://')):
+    target_url = str(input('Insert full correct Url (including http:// or https://) Please: '))
+headers = {
+    "server": "reveals server info (usually identifies the web server software, e.g., Apache, Nginx)",
+    "X-Frame-Options": "prevents clickjacking by controlling whether this page can be embedded in an iframe",
+    "X-Powered-By": " reveals what tech the server uses, helps attackers ",  # e.g., "ASP.NET, PHP" – can leak server‑side stack info
+    "Strict-Transport-Security": "forces the browser to use HTTPS only (HSTS), mitigating downgrade attacks",
+    "X-Content-Type-Options": "prevents MIME‑type sniffing; usually set to 'nosniff' to avoid content‑type confusion",
+    "Content-Security-Policy": "HIGH RISK if missing or misconfigured; prevents XSS and other injection attacks by whitelisting allowed sources"
+}
 with requests.session() as session:
-     response = session.get(target_url)
-     for header in headers:
-        if header in response.headers:
-         print(f'{header}: {response.headers[header]}')
-        else:
-            print(f'{header}: MISSING, POTENTIAL SECURITY ISSUE.')
+    try:
+        response = session.get(target_url, timeout=10)
+        for header in headers:
+            if header in response.headers:
+                print(f'{header}: {response.headers[header]}')
+            else:
+                print(f'{header}: MISSING, {headers[header]}.')
+
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        print('Connection error/possible wrong domain or Timeout occurred.')
+         
